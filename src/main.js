b325cfc1c340f38b16fd52ec86a5548f41d6b4d8
Пимашев Владислав <mr.pimashev58@gmail.com>
2022-08-11 12:07:16 +0300

    Выполнил задание №5 урока №4

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import colorDirective from "./directives/color";
-
+import plugin from "./plugins/plugin";
 const app = createApp(App);
 
-app.directive("color", colorDirective);
-app.config.globalProperties.$filters = {
-  numberFormat(value, currency = "") {
-    return ("" + value).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + currency;
-  },
-};
+// app.directive("color", colorDirective);
+// app.config.globalProperties.$filters = {
+//   numberFormat(value, currency = "") {
+//     return ("" + value).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + currency;
+//   },
+// };
 app.mixin({
   created() {
     console.log("component created: " + this.$options.name);
@@ -22,4 +21,4 @@ app.mixin({
   },
 });
 
-app.mount("#app");
+app.use(plugin).mount("#app");
---
+++ src/plugins/plugin.js
@@ -0,0 +1,34 @@
+import { h } from "vue";
+import colorDirective from "../directives/color";
+export default {
+  install: (app) => {
+    app.directive("color", colorDirective);
+    app.config.globalProperties.$filters = {
+      numberFormat(value, currency = "") {
+        return (
+          ("" + value).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + currency
+        );
+      },
+    };
+    app.component("TodoRenderButton", {
+      data() {
+        return {
+          tag: "button",
+          tags: ["button", "div", "h1", "span"],
+        };
+      },
+      render() {
+        return h(this.tag, {
+          class: "btn btn-danger",
+          innerHTML: "change Tag",
+          onClick: this.changeTag,
+        });
+      },
+      methods: {
+        changeTag() {
+          this.tag = this.tags[Math.floor(Math.random() * this.tags.length)];
+        },
+      },
+    });
+  },
+};
